b18e9f2ca9f647ea3761f4022710d33e2918d45e
Eduardo A.M <eduardo.maciel@edu.univali.br>
2023-07-28 21:32:24 -0300

    Add url validation with zod

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,30 @@ import { FaLink } from "react-icons/fa6";
 import { HiOutlineExternalLink } from "react-icons/hi";
 import useShortener from "./hooks/useShortener";
 import clipboard from "./utils/clipboard";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const linkSchema = z.object({
+   link: z.string().url({ message: "É necessário uma URL válida" }),
+});
+
+export type Link = z.infer<typeof linkSchema>;
 
 export default function App() {
-   const { link, setLink, result, onSubmitLink } = useShortener();
+   const { result, onSubmitLink } = useShortener();
+
+   const {
+      register,
+      handleSubmit,
+      formState: { errors },
+   } = useForm({
+      resolver: zodResolver(linkSchema),
+      mode: "all",
+      defaultValues: {
+         link: "",
+      },
+   });
 
    return (
       <div className="relative bg-[#F8F5F2] text-[#232323] m-auto flex flex-col items-center justify-center h-screen bg-layout min-h-screen">
@@ -18,26 +39,29 @@ export default function App() {
                </h2>
             </div>
 
-            <div className="flex flex-col items-center w-full">
-               <div className="flex flex-row items-center mb-8 content-center w-full relative">
+            <div className="flex flex-col items-center w-full relative">
+               <form
+                  onSubmit={handleSubmit(onSubmitLink)}
+                  className="flex flex-row items-center mb-11 content-center w-full relative"
+               >
                   <div className="w-full relative">
                      <input
-                        value={link}
-                        onChange={(e) => setLink(e.target.value)}
-                        type="url"
+                        {...register("link")}
                         placeholder="Enter your URL"
                         className="m-0 text-lg outline-0 w-full h-[50px] pl-[50px] border-solid border-[#232323] border-[3px]"
-                        name="input"
                      />
                      <FaLink className="text-[#F45D48] absolute top-3 left-4 text-2xl" />
                   </div>
-                  <button
-                     onClick={onSubmitLink}
-                     className="text-[#F8F5F2] w-[150px] h-[50px] border-solid border-[#232323] border-l-0 border-[3px] p-0 bg-[#078080] m-0 text-lg font-bold cursor-pointer px-3 hover:bg-[#026060]"
-                  >
+                  <button className="text-[#F8F5F2] w-[150px] h-[50px] border-solid border-[#232323] border-l-0 border-[3px] p-0 bg-[#078080] m-0 text-lg font-bold cursor-pointer px-3 hover:bg-[#026060]">
                      Shorten
                   </button>
-               </div>
+               </form>
+
+               {errors.link && (
+                  <span className="absolute top-14 left-0 text-[#F45D48] font-semibold">
+                     {errors.link.message}
+                  </span>
+               )}
 
                <div className="flex flex-row items-center content-center w-full">
                   <div className="w-full relative">
--- src/hooks/useShortener.tsx
+++ src/hooks/useShortener.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import linkShortener from "../services/bitly";
 import { toast } from "react-toastify";
+import { Link } from "../App";
 
 export default function useShortener() {
-   const [link, setLink] = useState("");
    const [result, setResult] = useState("");
 
-   async function onSubmitLink() {
+   async function onSubmitLink({ link: userLink }: Link) {
       try {
-         const data = await linkShortener(link);
+         const data = await linkShortener(userLink);
          setResult(data.link);
       } catch (error) {
          toast.error("Ocorreu um erro");
@@ -16,10 +16,7 @@ export default function useShortener() {
    }
 
    return {
-      link,
-      setLink,
       result,
-      setResult,
       onSubmitLink,
    };
 }
